d44de5e301111e4ffeea706a10b44d084580ec79
Sam Kessler <sholomkessler1@gmail.com>
2021-01-30 20:36:54 -0500

    Added on death event

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Health : MonoBehaviour
 {
+    public UnityEvent onDeath;
     [SerializeField]
     private int currentHealth;
     public void Damage(int amount)
     {
         currentHealth -= amount;
-        Destroy(gameObject);
+        if (currentHealth <= 0)
+            Die();
+    }
+    public void Die()
+    {
+        onDeath.Invoke();
     }
 }
