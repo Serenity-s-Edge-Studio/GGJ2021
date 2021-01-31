541ef1bbf935e52d84da1970d9732bb8043e76fc
Sam Kessler <sholomkessler1@gmail.com>
2021-01-30 20:35:51 -0500

    Created enemy.cs

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Health), typeof(Animator), typeof(CapsuleCollider2D))]
+public class Enemy : MonoBehaviour
+{
+    private Health health;
+    private Rigidbody2D[] rigidbodies;
+    private Collider2D mainCollider;
+    private Animator animator;
+
+    private void Start()
+    {
+        health = GetComponent<Health>();
+        health.onDeath.AddListener(() => SetRagdollStatus(true));
+        health.onDeath.AddListener(() => Invoke("remove", 5));
+        rigidbodies = GetComponentsInChildren<Rigidbody2D>();
+        mainCollider = GetComponent<Collider2D>();
+        animator = GetComponent<Animator>();
+    }
+    private void SetRagdollStatus(bool enableRagdoll)
+    {
+        enableRagdoll = !enableRagdoll;
+        mainCollider.enabled = enableRagdoll;
+        animator.enabled = enableRagdoll;
+        foreach(Rigidbody2D rigidbody in rigidbodies)
+        {
+            rigidbody.isKinematic = enableRagdoll;
+        }
+    }
+    private void remove()
+    {
+        Destroy(gameObject);
+    }
+}
